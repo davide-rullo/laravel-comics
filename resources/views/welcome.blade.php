dbb629afe4b0ad53924c5ef853cdbefcff820091
davide-rullo <davide.rullo01@gmail.com>
2023-10-26 17:23:24 +0200

    add banner

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,4 +5,18 @@
 <div class="jumbo">
 
 </div>
+<div class="wrap">
+    <div class="content d-flex align-items-center">
+        <div class="container">
+
+            <h3 class="title mb-3">CURRENT SERIES</h3>
+
+            <div class="row row-cols-1 row-cols-sm-3 row-cols-md-4 row-cols-lg-6">
+
+
+
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
