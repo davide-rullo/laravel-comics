f150d9217ee064ad6b9c492b3d9e45e8bd41aff5
davide-rullo <davide.rullo01@gmail.com>
2023-10-26 17:40:54 +0200

    add card loop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,12 @@
             <h3 class="title mb-3">CURRENT SERIES</h3>
 
             <div class="row row-cols-1 row-cols-sm-3 row-cols-md-4 row-cols-lg-6">
-
+                @foreach ($comics as $comic)
+                <div class="col">
+                    <img src="{{$comic['thumb']}}" class="imgcard" alt="">
+                    <p>{{$comic['series']}}</p>
+                </div>
+                @endforeach
 
 
             </div>
