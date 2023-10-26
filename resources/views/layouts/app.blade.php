79c7b94a645a124d020fe1882ceb87a12af7fe27
davide-rullo <davide.rullo01@gmail.com>
2023-10-26 15:19:40 +0200

    add app.blade file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC-Comics</title>
+</head>
+
+<body>
+    @include('partials.header')
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        Footer
+    </footer>
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <nav class="navbar d-flex justify-content-between pb-0">
+
+        <div class="logo">
+
+        </div>
+        <div class="nav-menu">
+            <ul class="navbar-nav d-flex flex-row gap-3">
+                <div class="nav-wrap d-flex justify-content-center align-items-center">
+                    <li class="nav-item">
+                        <h6>prova</h6>
+                    </li>
+                    <li class="nav-item">
+                        <h6>prova</h6>
+                    </li>
+                    <li class="nav-item">
+                        <h6>prova</h6>
+                    </li>
+                    <li class="nav-item">
+                        <h6>prova</h6>
+                    </li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+</header>
